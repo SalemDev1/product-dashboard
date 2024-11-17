12011d6f10567a0853d73618380ceebc52ff6505
Mamun Salem <salemtwin2@gmail.com>
2024-11-16 21:18:03 -0500

    Set up App component with initial product list

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import ProductList from './ProductList';
+import AddProductForm from './AddProductForm';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Default product list
+  const [products, setProducts] = useState([
+    { id: 1, name: 'Grand Theft Auto VI', price: 80, description: 'A highly antcipated game of 2025 (the coder who coded this is excited' },
+    { id: 2, name: 'Tom Ford Ombre Leather', price: 150, description: 'he sensuality of rich black leather is texturized with patchouli and vetiver, while floral tones of jasmine sambac conjure wild desert beauty' },
+    { id: 3, name: 'PS5 Pro', price: 700, description: 'a gaming console that offers enhanced performance, graphics, and storage' },
+
+  // Function in order to add a new product (Add ProductForm)
+  const addProduct = (newProduct) => {
+    setProducts([...products, newProduct]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Product Dashboard</h1>
+      <ProductList products={products} />
+      <AddProductForm onAddProduct={addProduct} />
+    </div>
+  );
 }
 
-export default App
+export default App;
