c9a66e47522e193784c934d344242c240ec85c45
Mamun Salem <salemtwin2@gmail.com>
2024-11-16 23:10:52 -0500

    Implement addProduct function and pass data between App and AddProductForm

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react';
 import ProductList from './ProductList';
 import AddProductForm from './AddProductForm';
 
+
 function App() {
   // Default product list
   const [products, setProducts] = useState([
-    { id: 1, name: 'Grand Theft Auto VI', price: 80, description: 'A highly antcipated game of 2025 (the coder who coded this is excited' },
-    { id: 2, name: 'Tom Ford Ombre Leather', price: 150, description: 'he sensuality of rich black leather is texturized with patchouli and vetiver, while floral tones of jasmine sambac conjure wild desert beauty' },
+    { id: 1, name: 'Grand Theft Auto VI', price: 80, description: 'A highly antcipated game of 2025 (the coder who coded this is excited for this game :D' },
+    { id: 2, name: 'Tom Ford Ombre Leather', price: 150, description: 'The sensuality of rich black leather is texturized with patchouli and vetiver, while floral tones of jasmine sambac conjure wild desert beauty' },
     { id: 3, name: 'PS5 Pro', price: 700, description: 'a gaming console that offers enhanced performance, graphics, and storage' },
+]); 
 
   // Function in order to add a new product (Add ProductForm)
   const addProduct = (newProduct) => {
